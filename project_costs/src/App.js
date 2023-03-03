8beef0c4fa4e911789d4393ff7e5ca75d93a343d
sylvynhaarc@hotmail.com <you@example.com>
2023-03-03 11:56:39 -0600

    routes created

--- project_costs/src/App.js
+++ project_costs/src/App.js
@@ -1,10 +1,30 @@
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Home from "./componets/pages/Home"
+import Company from './componets/pages/Company'
+import Contact from './componets/pages/Contact'
+import NewProject from './componets/pages/NewProject'
 
 
 function App() {
+  
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+       <li>Home</li>
+       <li>Contact</li>
+
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Company" element ={<Company/>} />
+        <Route path="/Contact" element ={<Contact/>} />
+        <Route path="/NewProject'" element ={<NewProject/>} />
+      </Routes>
+   
+      <p>Footer</p>
+    
+
+</Router>
   );
 }
 
